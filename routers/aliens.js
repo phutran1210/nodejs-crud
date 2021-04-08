0c7110ad226d7064809a130cc336dd89e0f1ddb2
Ares <phutran121094@gmail.com>
2021-04-08 17:28:27 +0700

    waiting

---
+++ routers/aliens.js
@@ -0,0 +1,38 @@
+const express = require("express");
+const router = express.Router();
+const Alien = require("../models/alien");
+
+router.get("/", async (req, res) => {
+  try {
+    const aliens = await Alien.find();
+    res.json(aliens);
+  } catch (err) {
+    res.send(err, "err");
+  }
+});
+
+router.get("/:id", async (req, res) => {
+  try {
+    const aliens = await Alien.findById(req.params.id);
+    res.json(aliens);
+  } catch (err) {
+    res.send(err, "err");
+  }
+});
+
+router.post("/", async (req, res) => {
+  const aliens = new Alien({
+    name: req.body.name,
+    tech: req.body.tech,
+    sub: req.body.sub,
+  });
+
+  try {
+    const a1 = await aliens.save();
+    res.json(a1);
+  } catch (err) {
+    res.send(err, "err");
+  }
+});
+
+module.exports = router;
